b183d8add05df2f006228ca04669592dfacc82a3
KWON <pporotoss@gmail.com>
2016-11-15 22:38:57 +0900

    JdbcTemplate을 이용하여 데이터베이스에서 데이터 조회하기

--- src/main/java/com/example/SpringBootStudyApplication.java
+++ src/main/java/com/example/SpringBootStudyApplication.java
@@ -1,10 +1,14 @@
 package com.example;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -22,11 +26,15 @@ public class SpringBootStudyApplication implements CommandLineRunner {	// Spring
 	
 	@Override
 	public void run(String... arg0) throws Exception {	// SpringApplication.run() 에서 수행해야 할 내용을 정의.
-		String sql = "SELECT :a + :b";
+		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
 		SqlParameterSource param = new MapSqlParameterSource()
-				.addValue("a", 100)
-				.addValue("b", 200);
-		Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+				.addValue("id", 1);
+		Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
+			@Override
+			public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
+			}
+		});
 		
 		System.out.println("result = "+result);
 	}
