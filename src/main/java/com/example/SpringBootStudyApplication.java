14f492f25b9008635c0dc7692887ec11e4ee05a9
KWON <pporotoss@gmail.com>
2016-11-15 22:41:46 +0900

    RowMapper를 람다식으로 구현

--- src/main/java/com/example/SpringBootStudyApplication.java
+++ src/main/java/com/example/SpringBootStudyApplication.java
@@ -1,20 +1,16 @@
 package com.example;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import com.example.domain.Customer;
-import com.example.service.CustomerService;
 
 
 @SpringBootApplication	// 다양한 스프링 관련 설정을 자동으로 해주는 어노테이션. @Configuration + @EnableAutoConfiguration + @ComponentScan
@@ -29,12 +25,9 @@ public class SpringBootStudyApplication implements CommandLineRunner {	// Spring
 		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
 		SqlParameterSource param = new MapSqlParameterSource()
 				.addValue("id", 1);
-		Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
-			@Override
-			public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
-				return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
-			}
-		});
+		Customer result = jdbcTemplate.queryForObject(sql, param, 
+				(rs, rowNum) -> new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"))
+		);
 		
 		System.out.println("result = "+result);
 	}
