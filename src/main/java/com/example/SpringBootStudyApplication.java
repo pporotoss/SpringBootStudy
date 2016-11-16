0677d267c66787c65fb920e3241c236f60afa381
KWON <pporotoss@gmail.com>
2016-11-16 15:19:07 +0900

    logging 적용 및 jdbc로 데이터 조회

--- src/main/java/com/example/SpringBootStudyApplication.java
+++ src/main/java/com/example/SpringBootStudyApplication.java
@@ -11,25 +11,21 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
 
 
 @SpringBootApplication	// 다양한 스프링 관련 설정을 자동으로 해주는 어노테이션. @Configuration + @EnableAutoConfiguration + @ComponentScan
 public class SpringBootStudyApplication implements CommandLineRunner {	// SpringApplication.run() 에서 수행해야 할 내용을 정의하는 인터페이스를 구현.
 	
 	@Autowired
-	private NamedParameterJdbcTemplate jdbcTemplate;
+	private CustomerRepository customerRepository;
 
-	
 	@Override
 	public void run(String... arg0) throws Exception {	// SpringApplication.run() 에서 수행해야 할 내용을 정의.
-		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
-		SqlParameterSource param = new MapSqlParameterSource()
-				.addValue("id", 1);
-		Customer result = jdbcTemplate.queryForObject(sql, param, 
-				(rs, rowNum) -> new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"))
-		);
+		Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
+		System.out.println("created = "+created);
 		
-		System.out.println("result = "+result);
+		customerRepository.findAll().forEach(System.out::println);
 	}
 	
 	
--- src/main/java/com/example/repository/CustomerRepository.java
+++ src/main/java/com/example/repository/CustomerRepository.java
@@ -2,30 +2,57 @@ package com.example.repository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.domain.Customer;
 
 @Repository
+@Transactional
 public class CustomerRepository {
-	private final ConcurrentMap<Integer, Customer> customerMap = new ConcurrentHashMap<>();	// A Map providing thread safety and atomicity guarantees.
+	
+	@Autowired
+	private NamedParameterJdbcTemplate jdbcTemplate;
+	
+	private static final RowMapper<Customer> customerRowMapper = (rs, i) -> {
+		Integer id = rs.getInt("id");
+		String firstName = rs.getString("first_name");
+		String lastName = rs.getString("last_name");
+		return  new Customer(id, firstName, lastName);
+	};
 	
 	public List<Customer> findAll() {
-		return new ArrayList<>(customerMap.values());
+		String sql = "SELECT id, first_name, last_name FROM customers ORDER BY id";
+		List<Customer> customers = jdbcTemplate.query(sql, customerRowMapper);
+		return customers;
 	}
 	
 	public Customer findOne(Integer customerId) {
-		return customerMap.get(customerId);
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", customerId);
+		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
+		return jdbcTemplate.queryForObject(sql, param, customerRowMapper);
 	}
 	
 	public Customer save(Customer customer) {
-		return customerMap.put(customer.getId(), customer);
+		SqlParameterSource param = new BeanPropertySqlParameterSource(customer);
+		String sql = "UPDATE customers SET first_name = :firstName, last_name = :lastName";
+		if(customer.getId() == null) {
+			sql = "INSERT INTO customers(first_name, last_name) values(:firstName, :lastName)";
+		}
+		jdbcTemplate.update(sql, param);
+		return customer;
 	}
 	
 	public void delete(Integer customerId) {
-		customerMap.remove(customerId);
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", customerId);
+		String sql = "DELETE FROM customers WHERE id = :id";
+		jdbcTemplate.update(sql, param);
 	}
 }
